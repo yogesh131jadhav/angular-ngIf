0384370092aff08192320ddf3b026a356129a5fd
yogesh131jadhav <yogesh131jadhav@gmail.com>
2021-06-08 02:25:24 +0530

    Added use cases for ngTemplate, ngContainer and ngContent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ChildComponent } from '../child/child.component';
+import { ParentComponent } from '../parent/parent.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, ChildComponent, ParentComponent],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
